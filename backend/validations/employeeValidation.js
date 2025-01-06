960f11caa239b65f30a05bfa7b6d600ebf5c8086
priyakumar28 <129167379+priyakumar28@users.noreply.github.com>
2025-01-06 09:32:08 +0530

    initial commit

--- backend/validations/employeeValidation.js
+++ backend/validations/employeeValidation.js
@@ -6,13 +6,13 @@ const employeeValidation = {
       firstName: Joi.string().min(1).max(255).required(),
       lastName: Joi.string().min(1).max(255).required(),     
       age: Joi.number().min(1).max(100).required(),
-      position: Joi.string().min(2).max(255),
-      department: Joi.string().min(2).max(255),
-      address: Joi.string().min(2).max(1000),
+      position: Joi.string().min(1).max(255),
+      department: Joi.string().min(1).max(255),
+      address: Joi.string().min(1).max(1000),
       contact: Joi.string().pattern(/^\d{10}$/), 
       email: Joi.string().email(),  
-      city: Joi.string().min(2).max(255), 
-      state: Joi.string().min(2).max(255) ,
+      city: Joi.string().min(1).max(255), 
+      state: Joi.string().min(1).max(255) ,
       zip: Joi.string(),
     });
 
